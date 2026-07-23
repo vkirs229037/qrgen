d42ff9b85b9a4de013530e18f8d50de5a877f8b2
vkirs229037 <vova.kirsanov.03@mail.ru>
2026-07-23 16:32:37 +0300

    создание кодов в бэкенде

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import qrcode
+import os
+
+DATA_PATH = "./data"
+
+def get_latest():
+    fpath = DATA_PATH + "/latest"
+    if not os.path.isfile(fpath):
+        return 1
+    with open(DATA_PATH + "/latest", "r") as f:
+        return int(f.read())
+
+def update_latest(idx):
+    with open(DATA_PATH + "/latest", "w") as f:
+        f.write(str(idx + 1))
 
 app = Flask(__name__)
 
@@ -9,6 +24,28 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    if request.method == "POST":
+        content = request.form.get("content")
+        if not content:
+            print(request.form)
+            # TODO тут ошибка 
+            return redirect("/")
+        qr = qrcode.QRCode(
+            version=None,
+            error_correction=qrcode.constants.ERROR_CORRECT_M,
+            box_size=15,
+            border=4
+        )
+        qr.add_data(content)
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
+        latest_idx = get_latest()
+        img.save(DATA_PATH + f"/{latest_idx}.png")
+        update_latest(latest_idx)
+        return redirect(f"/view/{latest_idx}")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
