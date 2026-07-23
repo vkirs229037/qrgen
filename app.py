12e3b6b504e930f16dd8d3b049273fcf555d70fd
vkirs229037 <vova.kirsanov.03@mail.ru>
2026-07-23 18:21:01 +0300

    адекватная обработка ошибок

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, send_from_directory
+from flask import Flask, render_template, request, redirect, send_from_directory, flash
 import qrcode
 import os
 
@@ -16,6 +16,7 @@ def update_latest(idx):
         f.write(str(idx + 1))
 
 app = Flask(__name__)
+app.secret_key = "test"
 
 @app.route("/")
 def home():
@@ -30,8 +31,7 @@ def create():
     if request.method == "POST":
         content = request.form.get("content")
         if not content:
-            print(request.form)
-            # TODO тут ошибка 
+            flash("Содержимое не может быть пустым")
             return redirect("/")
         qr = qrcode.QRCode(
             version=None,
@@ -51,13 +51,20 @@ def create():
 def view(id):
     fpath = DATA_PATH + f"/{id}.png"
     if not os.path.isfile(fpath):
-        # TODO тут ошибка
-        return redirect("/")
+        return render_template("notfound.html"), 404
     return render_template("view.html", fpath=f"{id}.png", id=id)
 
 @app.route("/data/<path:filename>")
 def get_file(filename):
     return send_from_directory(DATA_PATH, filename, as_attachment=True)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("error.html", error="Страница не найдена!", code=404)
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template("error.html", error="Произошла внутренняя ошибка сервера", code=500)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
